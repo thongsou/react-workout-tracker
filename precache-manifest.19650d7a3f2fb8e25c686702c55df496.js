45a5a3f07b2cdb0128553a3d9076731834379598
Tony "Assume" Yang <TonyAssumeYang@Tonys-MacBook-Air.local>
2020-07-15 00:59:55 -0500

    Updates

---
+++ precache-manifest.19650d7a3f2fb8e25c686702c55df496.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7b636e6ae367817f25844bc6f75f56",
+    "url": "/react-workout-tracker/index.html"
+  },
+  {
+    "revision": "c39d1d4ce6f16f18d06c",
+    "url": "/react-workout-tracker/static/css/main.2e4f6d15.chunk.css"
+  },
+  {
+    "revision": "7aa81393ad241ebb0610",
+    "url": "/react-workout-tracker/static/js/2.931dcb9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-workout-tracker/static/js/2.931dcb9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39d1d4ce6f16f18d06c",
+    "url": "/react-workout-tracker/static/js/main.1ea2e46f.chunk.js"
+  },
+  {
+    "revision": "1760cab622d9ea095f4a",
+    "url": "/react-workout-tracker/static/js/runtime-main.55d35a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-workout-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-workout-tracker/precache-manifest.19650d7a3f2fb8e25c686702c55df496.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-workout-tracker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
